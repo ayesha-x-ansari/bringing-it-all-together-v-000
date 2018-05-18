0064148c33dcbe9cea8ecbb703da6e6984aad22b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 17:32:44 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,75 @@
+#require_relative "../config/environment.rb"
+class Dog
+
+  attr_accessor  :id, :name, :breed
+
+  def initialize(id = nil, name, bredd)
+    @id = id
+    @name = name
+    @breed  = breed
+  end
+
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+        SQL
+
+        DB[:conn].execute(sql, self.name, self.breed)
+
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name =  row[1]
+    breed = row[2]
+    new_dog =   Dog.new(id, name, grade)                   # return the newly created instance
+  end
+
+  def self.create(name, breed)
+    dog = Student.new(name, breed)
+    dog.save
+  end
+
+ def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.find_by_name(name)
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+end
